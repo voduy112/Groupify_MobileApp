80a2b1109a2e3e6e22d0830b036a457971ce90dd
voduy112 <duyvo1122003@gmail.com>
2025-05-12 09:37:49 +0700

    Khoi tao du an

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const app = express();
+const Server = require('http').createServer(app);
+dotenv.config();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 5000;
+
+Server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
