9ff7e836ff7653583d38b20381f9c7c8b8e27d7b
voduy112 <duyvo1122003@gmail.com>
2025-05-12 09:49:20 +0700

    Ket noi DB

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const { connect } = require('http2');   
+const connectDB = require('./config/MongoDB');
+
 
 const app = express();
 const Server = require('http').createServer(app);
@@ -12,6 +15,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+connectDB();
 
 Server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
