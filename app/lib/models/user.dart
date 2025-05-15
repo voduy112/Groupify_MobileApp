7a5cb590476cee5c5b4e5cc299fd24c73c733284
voduy112 <duyvo1122003@gmail.com>
2025-05-15 22:43:54 +0700

    model user

--- app/lib/models/user.dart
+++ app/lib/models/user.dart
@@ -2,29 +2,35 @@ class User {
   String? id;
   String? username;
   String? email;
+  String? password;
   String? phoneNumber;
   String? profilePicture;
   String? token;
   String? refreshToken;
+  String? fcmToken = '';
 
   User({
     this.id,
     this.username,
     this.email,
+    this.password,
     this.phoneNumber,
     this.profilePicture,
     this.token,
     this.refreshToken,
+    this.fcmToken,
   });
 
   User.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     username = json['username'];
     email = json['email'];
+    password = json['password'];
     phoneNumber = json['phoneNumber'];
     profilePicture = json['profilePicture'];
     token = json['token'];
     refreshToken = json['refreshToken'];
+    fcmToken = json['fcmToken'] ?? '';
   }
 
   Map<String, dynamic> toJson() {
@@ -32,10 +38,12 @@ class User {
       'id': id,
       'username': username,
       'email': email,
+      'password': password,
       'phoneNumber': phoneNumber,
       'profilePicture': profilePicture,
       'token': token,
       'refreshToken': refreshToken,
+      'fcmToken': fcmToken ?? '',
     };
   }
 }
