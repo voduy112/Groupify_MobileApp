44a4d66232e6b2d96fc9ba3d37e3d5abc2e9e2c4
voduy112 <duyvo1122003@gmail.com>
2025-05-21 08:11:44 +0700

    TitleApp global

---
+++ app/lib/core/widgets/title_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TitleApp extends StatelessWidget {
+  final String title;
+  final BuildContext context;
+
+  const TitleApp({super.key, required this.title, required this.context});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Text(title, style: Theme.of(context).textTheme.titleMedium),
+      ),
+    );
+  }
+}
