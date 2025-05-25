609ce51ec7ac5e508d8c52981f6fd0eea77f2312
voduy112 <duyvo1122003@gmail.com>
2025-05-25 21:53:03 +0700

    thêm otp

--- api/routes/group.js
+++ api/routes/group.js
@@ -1,23 +1,18 @@
-const groupController = require('../controllers/groupController.js');
+const groupController = require("../controllers/groupController.js");
 const express = require("express");
 const router = express.Router();
 
-const {upload} = require ("../config/Multer")
+const { upload } = require("../config/Multer");
 
+router.get("/:id", groupController.getGroupById);
+router.get("/", groupController.getAllGroup);
+router.delete("/:id", groupController.deleteGroup);
+router.post("/", groupController.createGroup);
+router.post("/join", groupController.joinGroupByCode);
+router.get("/user/:id", groupController.getAllGroupByUserId);
 
-router.get('/:id', groupController.getGroupById);
-router.get('/', groupController.getAllGroup);
-router.delete('/:id', groupController.deleteGroup);
-router.post('/', groupController.createGroup);
-router.post('/join', groupController.joinGroupByCode);
-
-
-
-router.put('/:id',
-    upload.single('image'),
-    groupController.updateGroup
-)
-router.post('/leave', groupController.leaveGroup);
-router.get('/members/:id', groupController.getGroupMembers); //id la id cua group
+router.put("/:id", upload.single("image"), groupController.updateGroup);
+router.post("/leave", groupController.leaveGroup);
+router.get("/members/:id", groupController.getGroupMembers); //id la id cua group
 
 module.exports = router;
