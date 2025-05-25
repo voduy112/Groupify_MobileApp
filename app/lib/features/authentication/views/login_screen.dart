18c74f0e85ccd630d10345f57188b9c40508ee78
voduy112 <duyvo1122003@gmail.com>
2025-05-25 21:56:05 +0700

    Refactor LoginScreen to use StatefulWidget and implement form validation with email and password checks. Enhanced user feedback with SnackBars for login errors and email verification prompts.

--- app/lib/features/authentication/views/login_screen.dart
+++ app/lib/features/authentication/views/login_screen.dart
@@ -5,60 +5,84 @@ import '../../../features/authentication/providers/user_provider.dart';
 import '../../../features/home/views/home_screen.dart';
 import '../../../core/widgets/main_scaffold.dart';
 import 'package:go_router/go_router.dart';
+import '../../../core/utils/validate.dart';
+import 'otp_verification_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-
-  LoginScreen({super.key});
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Đăng nhập'),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Đăng nhập'),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Đăng nhập',
+                    style: Theme.of(context).textTheme.titleLarge),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(labelText: 'Email'),
+                  validator: Validate.email,
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(labelText: 'Mật khẩu'),
+                  obscureText: true,
+                  validator: Validate.password,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      final authProvider = context.read<AuthProvider>();
+                      final error = await authProvider.login(
+                        emailController.text,
+                        passwordController.text,
+                      );
+                      final user = authProvider.user;
+                      if (error == null && user != null) {
+                        context.go('/home');
+                      } else if (error != null &&
+                          error
+                              .toString()
+                              .contains('Please verify your email')) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Vui lòng xác thực email của bạn'),
+                          ),
+                        );
+                        context.go('/otp-verify', extra: emailController.text);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Đăng nhập thất bại!')),
+                        );
+                      }
+                    }
+                  },
+                  child: Text('Đăng nhập'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.go('/register');
+                  },
+                  child: Text('Đăng ký'),
+                ),
+              ],
             ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Mật khẩu',
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final authProvider = context.read<AuthProvider>();
-                await authProvider.login(
-                  emailController.text,
-                  passwordController.text,
-                );
-
-                final user = authProvider.user;
-                print(user!.id);
-                if (user != null && user.id != null) {
-                  context.read<UserProvider>().setUserId(user.id!);
-                  context.go('/home');
-                }
-              },
-              child: Text('Đăng nhập'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.go('/register');
-              },
-              child: Text('Đăng ký'),
-            ),
-          ],
+          ),
         ),
       ),
     );
