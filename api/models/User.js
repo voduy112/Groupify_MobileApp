e22b5a88e67f2c87880fa8fe3732fddffdefac88
voduy112 <duyvo1122003@gmail.com>
2025-05-25 21:52:32 +0700

    thêm trường otp

--- api/models/User.js
+++ api/models/User.js
@@ -1,40 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true, 
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     phoneNumber: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     profilePicture: {
-        type: String,
-        default: 'default.jpg',
+      type: String,
+      default: "default.jpg",
     },
     bio: {
-        type: String,
-        default: '',
+      type: String,
+      default: "",
     },
     role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user',
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
     refreshToken: {
-        type: String,
-        default: '',
+      type: String,
+      default: "",
     },
-}, { timestamps: true });
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    otp: {
+      type: String,
+      default: "",
+    },
+    otpExpires: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
