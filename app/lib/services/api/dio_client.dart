7f2ca9737235356015d67da23cb6a5720fac6ac5
voduy112 <duyvo1122003@gmail.com>
2025-05-27 08:27:30 +0700

    Add DioClient with interceptors and update main.dart initialization

--- app/lib/services/api/dio_client.dart
+++ app/lib/services/api/dio_client.dart
@@ -1,12 +1,19 @@
 import 'package:dio/dio.dart';
 
 class DioClient {
-  
-  static final Dio _dio = Dio(BaseOptions(
-    baseUrl: 'http://192.168.1.175:5000',
-    connectTimeout: const Duration(seconds: 10),
-    receiveTimeout: const Duration(seconds: 10),
-  ));
+  static late Dio _dio;
+
+  static void createInterceptors() {
+    _dio = Dio(BaseOptions(
+      baseUrl: 'http://10.0.2.2:5000',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+    ));
+    _dio.interceptors.add(LogInterceptor(
+      request: true,
+      error: true,
+    ));
+  }
 
   static Dio get instance => _dio;
 }
