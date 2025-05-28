5f6c5c92a80f749f7dfd1e7c3fd329033a0b784a
vietsu23 <nguyenvovietsu@gmail.com>
2025-05-28 09:54:03 +0700

    hien thi va kiem tra quiz

--- app/lib/features/quiz/providers/quiz_provider.dart
+++ app/lib/features/quiz/providers/quiz_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../services/quiz_service.dart';
 import '../../../models/quiz.dart';
+import '../../../models/resultquiz.dart';
 
 class QuizProvider extends ChangeNotifier {
   final QuizService _quizService = QuizService();
@@ -15,6 +16,20 @@ class QuizProvider extends ChangeNotifier {
   bool get isLoading => _isLoading;
   String? get error => _error;
 
+  Map<String, dynamic>? _result;
+  bool _isSubmitting = false;
+  
+
+  Map<String, dynamic>? get result => _result;
+  bool get isSubmitting => _isSubmitting;
+
+  List<ResultQuiz> _userResults = [];
+  bool _isFetchingResults = false;
+
+  List<ResultQuiz> get userResults => _userResults;
+  bool get isFetchingResults => _isFetchingResults;
+
+
   Future<void> fetchQuizzesByGroupId(String groupId) async {
     _isLoading = true;
     _error = null;
@@ -26,6 +41,25 @@ class QuizProvider extends ChangeNotifier {
       _error = e.toString();
     } finally {
       _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+   Future<void> submitQuiz(String quizId, String userId, List<Map<String, dynamic>> answers) async {
+    _isSubmitting = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _result = await _quizService.submitQuizAnswers(
+        quizId: quizId,
+        userId: userId,
+        answers: answers,
+      );
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isSubmitting = false;
       notifyListeners();
     }
   }
@@ -43,6 +77,32 @@ class QuizProvider extends ChangeNotifier {
       _isLoading = false;
       notifyListeners();
     }
+  }
+
+  Future<void> fetchResultsByQuizIdAndUserId({
+    required String quizId,
+    required String userId,
+  }) async {
+    _isFetchingResults = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _userResults = await _quizService.getResultsByQuizIdAndUserId(
+        quizId: quizId,
+        userId: userId,
+      );
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isFetchingResults = false;
+      notifyListeners();
+    }
+  }
+
+  void clearResults() {
+    _userResults = [];
+    notifyListeners();
   }
 
   void clearSelectedQuiz() {
