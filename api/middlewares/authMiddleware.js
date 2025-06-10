2ee48ec5f2b6690f2b5a1d22c71ed8753e090231
voduy112 <duyvo1122003@gmail.com>
2025-06-10 09:02:01 +0700

    Cấu trúc lại authMiddleware để cải thiện xác minh mã thông báo và xử lý lỗi. Đã thay đổi việc trích xuất mã thông báo để sử dụng tiêu đề ủy quyền và cập nhật thông báo phản hồi để mô tả rõ hơn.

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,24 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = {
   verifyToken: (req, res, next) => {
-    const token = req.headers.token;
-
-    if (token) {
-      const accessToken = token.split(" ")[1];
-      console.log("Access Token: ", accessToken);
-      jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.split(" ")[1]; // Bearer token
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
-          return res.status(403).json("Token is not valid!");
+          return res.status(403).json({ message: "Token is not valid!" });
         }
-        req.user = user; // gán user vào request
-        // console.log("User: ", user);
+        req.user = user;
         next();
       });
     } else {
-      return res.status(401).json("You are not authenticated!");
+      return res.status(401).json({ message: "You are not authenticated!" });
     }
-  }
+  },
 };
 
 module.exports = authMiddleware;
