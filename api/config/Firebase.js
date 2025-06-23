509cde389cf4490f1fd9c6e57b93739034dc9b2d
voduy112 <duyvo1122003@gmail.com>
2025-06-23 10:41:34 +0700

    sửa định dạng firebase

--- api/config/Firebase.js
+++ api/config/Firebase.js
@@ -1,6 +1,8 @@
 const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); // đường dẫn tới file bạn vừa tải
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
